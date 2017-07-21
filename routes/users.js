207c168a18b695f8abf27fdf25fa5d56f0e7f6da
Tucker Triggs <tucker.triggs@yahoo.com>
2017-07-21 10:34:59 -0600

    validation is working

--- routes/users.js
+++ routes/users.js
@@ -22,12 +22,76 @@ router.post('/' , (req, res, next) => {
   let email = req.body.users.email;
   let phone = req.body.users.phone;
 
-  if (!email || email.trim() === '') {
+  if (!firstName || firstName.trim() === '') {
+   const err = new Error('First name must not be blank');
+   err.status = 400;
+
+   return next(err);
+ }
+
+ if (!lastName || lastName.trim() === '') {
+  const err = new Error('Last name must not be blank');
+  err.status = 400;
+
+  return next(err);
+}
+
+if (!username || username.trim() === '') {
+ const err = new Error('Username must not be blank');
+ err.status = 400;
+
+ return next(err);
+}
+
+if (username.length < 6) {
+ const err = new Error('Username must be at least 6 characters');
+ err.status = 400;
+
+ return next(err);
+}
+
+if (!isNaN(+username[0])) {
+ const err = new Error('Username must start with a letter');
+ err.status = 400;
+
+ return next(err);
+}
+
+if (username.search(/[.,#!$%&*;:{}=\-_`~()]/) > 0) {
+ const err = new Error('Username must have no punctuation');
+ err.status = 400;
+
+ return next(err);
+}
+
+if (!email || email.trim() === '') {
    const err = new Error('Email must not be blank');
    err.status = 400;
 
    return next(err);
  }
+
+ if (!email.includes('@') && !email.includes('.')) {
+  const err = new Error('Email must be in email format');
+  err.status = 400;
+
+  return next(err);
+}
+
+if (/^\d{10}$/.test(phone)) {
+ const err = new Error('Phone number must be 10 digits');
+ err.status = 400;
+
+ return next(err);
+}
+
+if (isNaN(+phone)) {
+ const err = new Error('Phone number must contain all numbers');
+ err.status = 400;
+
+ return next(err);
+}
+
 
 
 
